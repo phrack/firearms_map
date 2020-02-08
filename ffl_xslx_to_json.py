ca2cdad95312950a5c551c4899983356a38671f8
phrack <code.realm@gmail.com>
2020-02-08 16:38:47 -0500

    Fix processing status message

--- ffl_xslx_to_json.py
+++ ffl_xslx_to_json.py
@@ -78,6 +78,6 @@ with open('ffl-list-pennsylvania.json', 'w') as f:
         count+=1
 
         if count % 100 == 0:
-            print('Processed ' + count + 'entries')
+            print('Processed ' + str(count) + 'entries')
 
     f.write(']}')
